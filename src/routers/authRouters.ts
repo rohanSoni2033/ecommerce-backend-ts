3ce735c6718438c70c562565b40f6ef0d4c6e628
rohan soni <rohansoni54476@gmail.com>
2023-11-30 15:43:22 +0530

    add authentication routers

--- src/routers/authRouters.ts
+++ src/routers/authRouters.ts
@@ -2,19 +2,25 @@ import { Router } from 'express';
 import {
   createAccount,
   login,
+  verifyCode,
   verifyMobile,
   forgotPassword,
   resetPassword,
   passwordResetToken,
+  authorize,
+  changePassword,
+  verifyPasswordResetToken,
 } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/create-account', createAccount);
-router.post('/verify-mobile', verifyMobile);
+router.post('/verify-mobile', verifyCode, verifyMobile);
+
 router.post('/forgot-password', forgotPassword);
-router.post('/password-reset-token', passwordResetToken);
-router.patch('/reset-password', resetPassword);
+router.post('/password-reset-token', verifyCode, passwordResetToken);
+router.post('/reset-password', verifyPasswordResetToken, resetPassword);
+router.post('/change-password', authorize, changePassword, resetPassword);
 
 export default router;
