aa85bec5cc4e29e0b0d4e4e3e6cd2c091fb8a4b7
rohan soni <rohansoni54476@gmail.com>
2023-11-30 15:42:30 +0530

    add authentication handlers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,17 @@
 import db from '../database/db.js';
 import GlobalError from '../utils/globalError.js';
-import asyncHandler from '../utils/asyncHandler.js';
+import { asyncHandler } from '../utils/methods.js';
 import { responseStatus, statusCode } from '../utils/constants.js';
-import { createHmac, randomInt } from 'node:crypto';
+import { createHmac } from 'node:crypto';
 import jwt from 'jsonwebtoken';
 import { ObjectId } from 'mongodb';
 import { UserTypes } from '../utils/constants.js';
-import Validate from '../utils/validate.js';
+import Validator from '../utils/validator.js';
 import bcrypt from 'bcrypt';
 import crypto from 'node:crypto';
+import responseMessage from '../utils/responseMessage.js';
 
-const sendVerificationCode = async (mobileNumber: string) => {
+const sendCode = async (mobileNumber: string) => {
   const verificationCode = crypto.randomInt(123456, 987654);
   console.log(`${mobileNumber}, verification code = ${verificationCode}`);
 
@@ -24,57 +25,217 @@ const sendVerificationCode = async (mobileNumber: string) => {
   };
 };
 
-const hashData = (data: {}) => {
-  return createHmac('sha256', process.env.hashSecret!)
+const hashData = (data: any) => {
+  return createHmac('sha256', process.env.HASH_SECRET!)
     .update(JSON.stringify(data))
     .digest('hex');
 };
 
-const validateCreateAccount = (body: {}) => {
-  const validate = new Validate(body)
-    .field('name')
-    .required('Please provide the name')
-    .isString('Name must be a string')
-    .validate(value => value.length <= 24, 'name be maximum of 24 characters')
-    .field('mobileNumber')
-    .required('Please provide the mobile number')
-    .check(/^[0-9]{10}$/, 'Please provide a valid mobile number')
-    .field('password')
-    .required('Please provide the password')
-    .validate(
-      value => value.length >= 8,
-      'Password must be of at least 8 characters'
-    )
-    .field('email')
-    .isString('Email must be a string');
-
-  return validate.errors;
-};
-
 export const createAccount = asyncHandler(async (req, res, next) => {
-  const { name, mobileNumber, password } = req.body;
+  const { firstName, lastName, mobileNumber, password } = new Validator(
+    req.body
+  )
+    .field('firstName')
+    .required('Please enter your first name.')
+    .field('lastName')
+    .required('Please enter your last name.')
+    .field('mobileNumber')
+    .required('Please enter your mobile number.')
+    .check(/^[0-9]{10}$/, 'Please enter a valid mobile number.')
+    .field('password')
+    .required('Please enter the password.')
+    .execute();
 
-  const errors = validateCreateAccount(req.body);
+  const user = await db()
+    .collection('users')
+    .findOne({ mobileNumber }, { projection: { active: 1 } });
 
-  if (errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, errors));
+  if (user && user?.active) {
+    return next(
+      new GlobalError(
+        statusCode.BAD_REQUEST,
+        responseMessage.ACCOUNT_ALREADY_EXITS
+      )
+    );
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-  const { insertedId } = await db().collection('users').insertOne({
-    name,
+  if (user && !user?.active) {
+    await db()
+      .collection('users')
+      .updateOne(
+        { mobileNumber },
+        {
+          $set: {
+            firstName,
+            lastName,
+            password: hashedPassword,
+            passwordUpdatedAt: new Date(),
+          },
+        }
+      );
+  } else {
+    await db().collection('users').insertOne({
+      firstName,
+      lastName,
+      mobileNumber,
+      password: hashedPassword,
+      passwordUpdatedAt: new Date(),
+      createdAt: new Date(),
+      userType: UserTypes.CUSTOMER,
+      mobileNumberVerified: false,
+      active: false,
+    });
+  }
+
+  const { expiresAt, verificationCode } = await sendCode(mobileNumber);
+
+  const hash = hashData({ mobileNumber, verificationCode, expiresAt });
+
+  res.status(statusCode.OK).json({
+    status: 'success',
+    data: { mobileNumber, expiresAt, hash },
+    message: `A 6-digit verification code has been sent to ${mobileNumber}.`,
+  });
+});
+
+export const verifyCode = asyncHandler(async (req, res, next) => {
+  const { mobileNumber, verificationCode, expiresAt, hash } = new Validator(
+    req.body
+  )
+    .field('mobileNumber')
+    .required('mobile number is required.')
+    .field('verificationCode')
+    .required('Please enter your verification code.')
+    .isNumber('verification code must be a number')
+    .field('expiresAt')
+    .required('expires at is required.')
+    .isDate('expires at must be a date')
+    .field('hash')
+    .required('hash is required.')
+    .execute();
+
+  if (new Date() > new Date(expiresAt)) {
+    return next(
+      new GlobalError(statusCode.BAD_REQUEST, responseMessage.CODE_EXPIRES)
+    );
+  }
+
+  const userHash = hashData({ mobileNumber, verificationCode, expiresAt });
+
+  if (userHash !== hash) {
+    return next(
+      new GlobalError(statusCode.BAD_REQUEST, responseMessage.INCORRECT_CODE)
+    );
+  }
+  next();
+});
+
+export const verifyMobile = asyncHandler(async (req, res, next) => {
+  const { mobileNumber } = req.body;
+
+  const result = await db()
+    .collection('users')
+    .findOneAndUpdate(
+      { mobileNumber },
+      {
+        $set: {
+          mobileNumberVerified: true,
+          active: true,
+        },
+      },
+      {
+        returnDocument: 'after',
+      }
+    );
+
+  if (!result.value) {
+    return next(
+      new GlobalError(statusCode.NOT_FOUND, responseMessage.ACCOUNT_NOT_FOUND)
+    );
+  }
+
+  const authToken = jwt.sign(
+    {
+      userId: result.value?._id,
+    },
+    process.env.JWT_SECRET!,
+    {
+      expiresIn: '90d',
+    }
+  );
+
+  res.status(statusCode.OK).json({
+    status: responseStatus.SUCCESS,
+    data: {
+      authToken,
+    },
+  });
+});
+
+export const login = asyncHandler(async (req, res, next) => {
+  const { mobileNumber, password } = new Validator(req.body)
+    .field('mobileNumber')
+    .required('Please enter your mobile number.')
+    .check(/^[0-9]{10}$/, 'Please enter a valid mobile number.')
+    .field('password')
+    .required('Please enter your password.')
+    .execute();
+
+  const user = await db().collection('users').findOne({
     mobileNumber,
-    password: hashedPassword,
-    passwordUpdatedAt: new Date(),
-    createdAt: new Date(),
-    userType: UserTypes.CUSTOMER,
-    mobileNumberVerified: false,
+    active: true,
   });
 
-  const { expiresAt, verificationCode } = await sendVerificationCode(
-    mobileNumber
-  );
+  if (!user) {
+    return next(
+      new GlobalError(statusCode.BAD_REQUEST, responseMessage.ACCOUNT_NOT_FOUND)
+    );
+  }
+
+  const passwordMatched = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatched) {
+    return next(
+      new GlobalError(
+        statusCode.BAD_REQUEST,
+        responseMessage.INCORRECT_PASSWORD
+      )
+    );
+  }
+
+  const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+    expiresIn: '90d',
+  });
+
+  res.status(statusCode.OK).json({
+    status: responseStatus.SUCCESS,
+    data: {
+      authToken,
+    },
+  });
+});
+
+export const forgotPassword = asyncHandler(async (req, res, next) => {
+  const { mobileNumber } = new Validator(req.body)
+    .field('mobileNumber')
+    .required('Please enter the mobile number.')
+    .check(/^[0-9]{10}$/, 'Please enter a valid mobile number.')
+    .execute();
+
+  const user = await db().collection('users').findOne({
+    mobileNumber,
+    active: true,
+  });
+
+  if (!user) {
+    return next(
+      new GlobalError(statusCode.BAD_REQUEST, responseMessage.ACCOUNT_NOT_FOUND)
+    );
+  }
+
+  const { expiresAt, verificationCode } = await sendCode(mobileNumber);
 
   const hash = hashData({
     mobileNumber,
@@ -88,255 +249,19 @@ export const createAccount = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const verifyMobile = asyncHandler(async (req, res, next) => {
-  const {
-    mobileNumber,
-    expiresAt,
-    verificationCode,
-    hash: userHash,
-  } = req.body;
-
-  const validate = new Validate(req.body)
-    .field('mobileNumber')
-    .required('Please provide the mobile number')
-    .field('expiresAt')
-    .required('Please provide the expires at')
-    .isDate('Please provide a valid expires at')
-    .field('verificationCode')
-    .required('Please provide the verification code')
-    .isNumber('Please provide a valid verification code')
-    .field('hash')
-    .required('Please provide the hash');
-
-  if (validate.errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, validate.errors));
-  }
-
-  const hash = hashData({
-    mobileNumber,
-    verificationCode,
-    expiresAt,
-  });
-
-  if (userHash !== hash) {
-    return next(
-      new GlobalError(
-        statusCode.BAD_REQUEST,
-        'wrong verification code, try again'
-      )
-    );
-  }
-
-  if (new Date() > new Date(expiresAt)) {
-    return next(
-      new GlobalError(
-        statusCode.BAD_REQUEST,
-        'verification code expires, try again'
-      )
-    );
-  }
-
-  const result = await db()
-    .collection('users')
-    .findOneAndUpdate(
-      {
-        mobileNumber,
-      },
-      {
-        $set: {
-          mobileNumberVerified: true,
-        },
-      }
-    );
-
-  if (!result.value) {
-    return next(
-      new GlobalError(
-        statusCode.UNAUTHORIZED,
-        'User not found with this mobile number'
-      )
-    );
-  }
-
-  const token = jwt.sign(
-    {
-      userId: result.value?._id,
-    },
-    process.env.jwtSecret!,
-    {
-      expiresIn: '90d',
-    }
-  );
-
-  res.status(statusCode.OK).json({
-    status: responseStatus.SUCCESS,
-    data: {
-      token,
-    },
-  });
-});
-
-export const login = asyncHandler(async (req, res, next) => {
-  const { mobileNumber, password } = req.body;
-
-  const validate = new Validate(req.body)
-    .field('mobileNumber')
-    .required('Please provide the mobile number')
-    .check(/^[0-9]{10}$/, 'Please provide a valid mobile number')
-    .field('password')
-    .required('Please provide the password');
-
-  if (validate.errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, validate.errors));
-  }
-
-  const user = await db().collection('users').findOne({
-    mobileNumber,
-  });
-
-  if (!user) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, 'User not found'));
-  }
-
-  const passwordMatched = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatched) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, 'Password is wrong'));
-  }
-
-  if (user.verified) {
-    const token = jwt.sign(
-      {
-        userId: user._id,
-      },
-      process.env.jwtSecret!,
-      {
-        expiresIn: '90d',
-      }
-    );
-
-    res.status(statusCode.OK).json({
-      status: responseStatus.SUCCESS,
-      data: {
-        token,
-      },
-    });
-  } else {
-    const { expiresAt, verificationCode } = await sendVerificationCode(
-      mobileNumber
-    );
-
-    const hash = hashData({
-      mobileNumber,
-      verificationCode,
-      expiresAt,
-    });
-
-    res.status(statusCode.OK).json({
-      status: 'success',
-      data: { mobileNumber, expiresAt, hash },
-    });
-  }
-});
-
-export const forgotPassword = asyncHandler(async (req, res, next) => {
+export const passwordResetToken = asyncHandler(async (req, res, next) => {
   const { mobileNumber } = req.body;
 
-  const validate = new Validate(req.body)
-    .field('mobileNumber')
-    .required('Please provide the mobile number')
-    .check(/^[0-9]{10}$/, 'Please provide a valid mobile number');
-
-  if (validate.errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, validate.errors));
-  }
-
-  const user = db().collection('users').findOne({
-    mobileNumber,
-  });
-
-  if (!user) {
-    return next(
-      new GlobalError(
-        statusCode.BAD_REQUEST,
-        'User not found with this mobile number'
-      )
-    );
-  }
-
-  const { expiresAt, verificationCode } = await sendVerificationCode(
-    mobileNumber
-  );
-
-  const hash = hashData({
-    mobileNumber,
-    verificationCode,
-    expiresAt,
-  });
-
-  res.status(statusCode.OK).json({
-    status: 'success',
-    data: { verificationCode, mobileNumber, expiresAt, hash },
-  });
-});
-
-export const passwordResetToken = asyncHandler(async (req, res, next) => {
-  const {
-    mobileNumber,
-    expiresAt,
-    verificationCode,
-    hash: userHash,
-  } = req.body;
-
-  const validate = new Validate(req.body)
-    .field('mobileNumber')
-    .required('Please provide the mobile number')
-    .field('expiresAt')
-    .required('Please provide the expires at')
-    .isDate('Please provide a valid expires at')
-    .field('verificationCode')
-    .required('Please provide the verification code')
-    .isNumber('Please provide a valid verification code')
-    .field('hash')
-    .required('Please provide the hash');
-
-  if (validate.errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, validate.errors));
-  }
-
-  const hash = hashData({
-    mobileNumber,
-    verificationCode,
-    expiresAt,
-  });
-
-  if (userHash !== hash) {
-    return next(
-      new GlobalError(
-        statusCode.BAD_REQUEST,
-        'wrong verification code, try again'
-      )
-    );
-  }
-
-  if (new Date() > new Date(expiresAt)) {
-    return next(
-      new GlobalError(
-        statusCode.BAD_REQUEST,
-        'verification code expires, try again'
-      )
-    );
-  }
-
   const user = await db().collection('users').findOne({
     mobileNumber,
+    active: true,
   });
 
   if (!user) {
     return next(
       new GlobalError(
         statusCode.UNAUTHORIZED,
-        'User not found with this mobile number'
+        responseMessage.ACCOUNT_NOT_FOUND
       )
     );
   }
@@ -345,7 +270,7 @@ export const passwordResetToken = asyncHandler(async (req, res, next) => {
     {
       userId: user._id,
     },
-    process.env.jwtSecret!,
+    process.env.JWT_SECRET!,
     {
       expiresIn: '3min',
     }
@@ -359,41 +284,70 @@ export const passwordResetToken = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const resetPassword = asyncHandler(async (req, res, next) => {
-  const { passwordResetToken, newPassword } = req.body;
+export const changePassword = asyncHandler(async (req, res, next) => {
+  const { currentPassword } = new Validator(req.body)
+    .field('currentPassword')
+    .required('Please enter your current password.')
+    .field('newPassword')
+    .required('Please enter the new password')
+    .execute();
 
-  const validate = new Validate(req.body)
+  const passwordMatched = await bcrypt.compare(
+    currentPassword,
+    req.user.password
+  );
+
+  if (!passwordMatched) {
+    return next(
+      new GlobalError(
+        statusCode.BAD_REQUEST,
+        responseMessage.INCORRECT_PASSWORD
+      )
+    );
+  }
+  next();
+});
+
+export const verifyPasswordResetToken = asyncHandler(async (req, res, next) => {
+  const { passwordResetToken } = new Validator(req.body)
     .field('passwordResetToken')
     .required('Password rest token is required')
-    .isString('password must be string')
+    .isString('Password must be string')
     .field('newPassword')
-    .required('new password is required');
+    .required('Please enter the new password.')
+    .execute();
 
-  if (validate.errors.length > 0) {
-    return next(new GlobalError(statusCode.BAD_REQUEST, validate.errors));
-  }
+  const data = jwt.verify(passwordResetToken, process.env.JWT_SECRET!);
 
-  const data = jwt.verify(passwordResetToken, process.env.jwtSecret!) as {
-    userId: string;
+  const { userId } = data as { userId: string };
+
+  req.user = {
+    _id: userId,
   };
+  next();
+});
 
-  const userId = data.userId;
-
+export const resetPassword = asyncHandler(async (req, res, next) => {
+  const { newPassword } = req.body;
+  const userId = req.user._id;
   const hashedPassword = await bcrypt.hash(newPassword, 10);
 
   const user = await db()
     .collection('users')
     .updateOne(
       { _id: new ObjectId(userId) },
-      { $set: { password: hashedPassword } }
+      { $set: { password: hashedPassword, passwordUpdatedAt: new Date() } }
     );
 
   if (user.matchedCount < 1) {
-    return next(new GlobalError(statusCode.NOT_FOUND, 'User not found'));
+    return next(
+      new GlobalError(statusCode.NOT_FOUND, responseMessage.ACCOUNT_NOT_FOUND)
+    );
   }
 
-  res.status(statusCode.BAD_REQUEST).json({
+  res.status(statusCode.OK).json({
     status: 'success',
+    message: 'Your password has been successfully updated.',
   });
 });
 
@@ -402,30 +356,35 @@ export const authorize = asyncHandler(async (req, res, next) => {
 
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(
-      new GlobalError(
-        statusCode.UNAUTHORIZED,
-        'please login to access this route'
-      )
+      new GlobalError(statusCode.UNAUTHORIZED, responseMessage.UNAUTHORIZED)
     );
   }
 
-  const token = authorization.split(' ').at(1);
-  if (!token) {
-    return next(new GlobalError(statusCode.UNAUTHORIZED, 'invalid token'));
+  const authToken = authorization.split(' ').at(1);
+
+  if (!authToken) {
+    return next(
+      new GlobalError(statusCode.UNAUTHORIZED, responseMessage.INVALID_TOKEN)
+    );
   }
 
-  const decoded = jwt.verify(token, process.env.jwtSecret!) as {
-    userId: string;
-  };
+  const data = jwt.verify(authToken, process.env.JWT_SECRET!);
 
-  const usersCollection = db().collection('users');
+  const { userId } = data as { userId: string };
 
-  const user = await usersCollection.findOne({
-    _id: new ObjectId(decoded.userId),
-  });
+  const user = await db()
+    .collection('users')
+    .findOne({
+      _id: new ObjectId(userId),
+    });
 
   if (!user) {
-    return next(new GlobalError(statusCode.NOT_FOUND, 'user not found'));
+    return next(
+      new GlobalError(statusCode.NOT_FOUND, responseMessage.ACCOUNT_NOT_FOUND)
+    );
+  }
+
+  if (user.password.passwordUpdatedAt > new Date()) {
   }
 
   req.user = user;
@@ -436,10 +395,7 @@ export const accessPermission = (...users: UserTypes[]) => {
   return asyncHandler(async (req, res, next) => {
     if (!users.includes(req.user.userType)) {
       return next(
-        new GlobalError(
-          statusCode.UNAUTHORIZED,
-          'you cannot access this permission'
-        )
+        new GlobalError(statusCode.UNAUTHORIZED, responseMessage.CAN_NOT_ACCESS)
       );
     }
     next();
